fe033ae023ea60d06b88f4f2c0496c8119c9fd6d
Julian Nicholls <juliannicholls29@gmail.com>
2018-11-17 20:26:10 +0000

    Set Moment locale to GB

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import moment from 'moment';
+
 import App from './App';
+
+moment.locale('en-gb');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
