2f9cbb3389cb36b3bc75cd01e8d2c5721512b274
Julian Nicholls <juliannicholls29@gmail.com>
2018-11-18 21:57:11 +0000

    Add a subheader, with smaller text in card header

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-function Card({ header, headerClass, footer, children }) {
+function Card({ header, subheader, headerClass, footer, children }) {
   return (
     <div className="card">
-      <div className={`card__header ${headerClass}`}>{header}</div>
+      <div className={`card__header ${headerClass}`}>
+        {header}
+        {subheader && <small>{subheader}</small>}
+      </div>
       {children && <div className="card__content">{children}</div>}
       {footer && <div className="card__footer">{footer}</div>}
     </div>
