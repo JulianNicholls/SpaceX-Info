c1b4f45a1a77cfee9da7bc9f641e9b7dc8bc5322
Julian Nicholls <juliannicholls29@gmail.com>
2019-03-15 21:37:40 +0000

    Add loaders for all the sections

--- src/api.js
+++ src/api.js
@@ -1,13 +1,21 @@
 const API_BASE_URL = 'https://api.spacexdata.com/v3/';
 
-export async function loadLaunches() {
-  const response = await fetch(`${API_BASE_URL}launches`);
+async function loadSection(section) {
+  const response = await fetch(`${API_BASE_URL}${section}`);
 
   return await response.json();
 }
 
-export async function loadRockets() {
-  const response = await fetch(`${API_BASE_URL}rockets`);
-
-  return await response.json();
-}
+export const loadCapsules = async () => loadSection('capsules');
+export const loadCores = async () => loadSection('cores');
+export const loadDragons = async () => loadSection('dragons');
+export const loadHistory = async () => loadSection('history');
+export const loadInfo = async () => loadSection('info');
+export const loadLandingPads = async () => loadSection('landpads');
+export const loadLaunches = async () => loadSection('launches');
+export const loadLaunchPads = async () => loadSection('launchpads');
+export const loadMissions = async () => loadSection('missions');
+export const loadPayloads = async () => loadSection('payloads');
+export const loadRockets = async () => loadSection('rockets');
+export const loadRoadster = async () => loadSection('roadster');
+export const loadShips = async () => loadSection('ships');
