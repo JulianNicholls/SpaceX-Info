da3971b837ca8df37ca99c9cbc17529caa304ec8
Julian Nicholls <juliannicholls29@gmail.com>
2019-04-13 19:36:17 +0100

    Export loadSection from api.
    HoldingPage can then display the raw returned data.

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL = 'https://api.spacexdata.com/v3/';
 
-async function loadSection(section) {
+export async function loadSection(section) {
   const response = await fetch(`${API_BASE_URL}${section}`);
 
   return await response.json();
@@ -11,9 +11,9 @@ export const loadCores = async () => loadSection('cores');
 export const loadDragons = async () => loadSection('dragons');
 export const loadHistory = async () => loadSection('history');
 export const loadInfo = async () => loadSection('info');
+export const loadLaunchPads = async () => loadSection('launchpads');
 export const loadLandingPads = async () => loadSection('landpads');
 export const loadLaunches = async () => loadSection('launches');
-export const loadLaunchPads = async () => loadSection('launchpads');
 export const loadMissions = async () => loadSection('missions');
 export const loadPayloads = async () => loadSection('payloads');
 export const loadRockets = async () => loadSection('rockets');
